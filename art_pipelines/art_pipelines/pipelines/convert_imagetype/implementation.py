a93f106f008e3b92614204419d7b139e11e2efde
Aaron <aaronrmm@gmail.com>
2021-09-15 13:45:27 -0400

    feat: imagetype converter has expected stub methods

--- art_pipelines/art_pipelines/pipelines/convert_imagetype/implementation.py
+++ art_pipelines/art_pipelines/pipelines/convert_imagetype/implementation.py
@@ -1,7 +1,10 @@
-import os
 from PIL import Image
 
-from art_pipelines.cv_types import FiletypeImage
+from art_pipelines.art_pipelines.cv_types import FiletypeImage
+
+
+def load(config):
+    pass
 
 
 def transform(config, input_path, output_path):
@@ -9,3 +12,7 @@ def transform(config, input_path, output_path):
     print(to_type.name)
     original = Image.open(input_path)
     original.save(output_path, format=to_type.value["PIL"])
+
+
+def destroy(config):
+    pass
