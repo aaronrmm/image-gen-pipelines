8713d0ac1e68ad5a11ffd0709ba2e9d4047c9215
Aaron <aaronrmm@gmail.com>
2021-10-24 11:26:38 -0400

    feat: save_image saves numpy arrays

--- art_pipelines/art_pipelines/image_utils.py
+++ art_pipelines/art_pipelines/image_utils.py
@@ -1,18 +1,31 @@
 import os
+from typing import Union
 
+import numpy as np
 import PIL.Image
+from cv2 import cv2
 
 
-def save_image(image, save_dir, save_name):
-    save_dir = os.path.abspath(save_dir)
-    saved_path = None
-    if PIL.Image.isImageType(image):
-        extension = image.format
-        save_path = os.path.join(save_dir, f"{save_name}.{extension}")
-        image.save(save_path)
-    else:
-        raise Exception(f"object to save must be an image, but is a {type(image)}")
-    if saved_path:
+def save_image(
+    image: Union[PIL.Image.Image, np.ndarray],
+    save_path=None,
+    save_dir=None,
+    save_name=None,
+):
+    if type(image) is PIL.Image.Image:
+        if PIL.Image.isImageType(image):
+            if not save_path:
+                extension = image.format
+                save_dir = os.path.abspath(save_dir)
+                save_path = os.path.join(save_dir, f"{save_name}.{extension}")
+            image.save(save_path)
+        else:
+            raise Exception(f"object to save must be an image, but is a {type(image)}")
+    elif type(image) is np.ndarray:
+        cv2.imwrite(save_path, image)
+        extension = "png"
+        save_path = save_path or os.path.join(save_dir, f"{save_name}.{extension}")
+    if save_path:
         print(f"{save_name} saved to {save_path}")
 
 
