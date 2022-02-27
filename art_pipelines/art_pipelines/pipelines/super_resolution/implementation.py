2c1f90e274995bc7360c062a9ac160fd23238afc
Aaron <aaronrmm@gmail.com>
2022-02-26 19:00:44 -0500

    Fix misplaced error

--- art_pipelines/art_pipelines/pipelines/super_resolution/implementation.py
+++ art_pipelines/art_pipelines/pipelines/super_resolution/implementation.py
@@ -1,10 +1,4 @@
 # https://github.com/ChristophReich1996/Swin-Transformer-V2
-from typing import Any
-
-import torch
-from PIL import Image
-import os
-import sys
 
 from pipelines.super_resolution.magnifier import Magnifier
 
@@ -16,13 +10,14 @@ def load(config):
     print("loading...")
     print(config.keys())
     if "model_name" in config and config["model_name"] not in ['RealESRGAN_x4plus', 'RealESRNet_x4plus']:
-        print("unknown model name", config["model_name"])
         if config["model_name"].lower() in ["bsrgan"]:
             from .bsrgan import BsrganMagnifier
             singleton = BsrganMagnifier()
         elif config["model_name"].lower() in ["swinir"]:
             from .swinIR import SwinIrMagnifier
             singleton = SwinIrMagnifier()
+        else:
+            raise NotImplementedError(f"unknown model name{config['model_name']}")
     elif config["model_name"]:  # ['RealESRGAN_x4plus', 'RealESRNet_x4plus']
         from .bsrgan import BsrganMagnifier
         singleton = BsrganMagnifier()
